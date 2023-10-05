e6dac79796a9654edf0a6c48c52de2afb64a23fc
Doğukan Tuna <144160351+44shell@users.noreply.github.com>
2023-10-05 22:24:25 +0300

    Update sqlmapoto.py
    update

--- sqlmapoto.py
+++ sqlmapoto.py
@@ -4,6 +4,13 @@ import sys
 import subprocess
 
 def run_sqlmap(target):
+    ascii_yazi = """
+    | || | | || |  ___| |__   ___| | |
+    | || |_| || |_/ __| '_ \ / _ | | |
+    |__   _|__   _\__ | | | |  __| | |
+       |_|    |_| |___|_| |_|\___|_|_|
+    """
+    print(ascii_yazi)
     print("[*] Şu adrese saldırı başlatılıyor: {}".format(target))
     sqlmap_command = "sqlmap -u {} --risk=3 --level=5 --random-agent --user-agent -v3 --batch --threads=10 --dump".format(target)
     subprocess.call(sqlmap_command, shell=True)
